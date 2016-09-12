3fbf01d649db28c23213ec62e9eeb5a7c3b80aa6
Rumata Estorsky <rumata@sputnikchess.ru>
2016-09-12 22:47:02 +0300

    add scala test lib && first test

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := """leetcode"""
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  "org.scalactic" %% "scalactic" % "3.0.0",
+  "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+)
+
--- src/main/scala/easy/ContainsDuplicate.scala
+++ src/main/scala/easy/ContainsDuplicate.scala
@@ -3,14 +3,9 @@ package easy
 /**
  * Created by rumata on 8/19/15.
  */
-class ContainsDuplicate {
+object ContainsDuplicate {
 
   def containsDuplicate(nums: Iterable[Int]) = nums.groupBy(identity).find(_._2.size > 1).nonEmpty
 
-
-  def test = {
-    assert(containsDuplicate(Array(1,3,4,5,6,7,9,10,10,11)) == true)
-    assert(containsDuplicate(Array(1,3,4,5,6,7)) == false)
-  }
 
 }
